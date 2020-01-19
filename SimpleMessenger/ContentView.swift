10d9a44515b29af3eccfd60f9c4328b4ab19f538
dmitrydnl <48892069+Captious99@users.noreply.github.com>
2020-01-19 20:22:26 +0300

    Start project

---
+++ SimpleMessenger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleMessenger
+//
+//  Created by Dmitry Danilin on 19.01.2020.
+//  Copyright © 2020 Dmitry Danilin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
